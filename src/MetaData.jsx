d824cd0ba0544cc8748394450355d131f730c38d
AJ Westley <alexanderjwestley@gmail.com>
2024-01-24 17:43:55 +0200

    metadata changes

--- src/MetaData.jsx
+++ src/MetaData.jsx
@@ -8,7 +8,7 @@ export function OGTags() {
             <meta property="og:site_name" content="AJ Westley" />
             <meta property="og:url" content="https://ajwestley.me/home" />
             <meta name="og:image" content="https://live.staticflickr.com/65535/53483481862_706d057b51_h.jpg"/>
-            <meta property="og:image:type" content="image/jpeg"/>
+            <meta property="og:image:type" content="image/jpg"/>
             <meta property="og:image:width" content="1600"/>
             <meta property="og:image:height" content="585"/>
         </Helmet>
