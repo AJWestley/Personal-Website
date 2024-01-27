28e0d4677ba80f9fef501992beec7153cb0897a0
AJ Westley <alexanderjwestley@gmail.com>
2024-01-27 12:06:55 +0200

    Optimizations

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,29 +7,29 @@ export function NavBar() {
     }
 
     return (
-        <nav className="navbar">
-        <div className="navbar__container">
-            <a href="/" id="navbar__logo">AJ Westley</a>
-            <div className="navbar__toggle" id="mobile-menu" onClick={()=>{toggleActive()}}>
-                <span className="bar"></span>
-                <span className="bar"></span>
-                <span className="bar"></span>
+        <nav className="navbar" rel='preload'>
+            <div className="navbar__container">
+                <a href="/" id="navbar__logo">AJ Westley</a>
+                <div className="navbar__toggle" id="mobile-menu" onClick={()=>{toggleActive()}}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
+                <ul className="navbar__menu">
+                    <li className="navbar__item">
+                        <a href="/" className="navbar__links">Home</a>
+                    </li>
+                    <li className="navbar__item">
+                        <a href="/about" className="navbar__links">About</a>
+                    </li>
+                    <li className="navbar__item">
+                        <a href="/work" className="navbar__links">Work</a>
+                    </li>
+                    <li className="navbar__item">
+                        <a href="mailto:alexanderjwestley@gmail.com?subject=AJ Westley Contact Form" className="navbar__links">Contact</a>
+                    </li>
+                </ul>
             </div>
-            <ul className="navbar__menu">
-                <li className="navbar__item">
-                    <a href="/" className="navbar__links">Home</a>
-                </li>
-                <li className="navbar__item">
-                    <a href="/about" className="navbar__links">About</a>
-                </li>
-                <li className="navbar__item">
-                    <a href="/work" className="navbar__links">Work</a>
-                </li>
-                <li className="navbar__item">
-                    <a href="mailto:alexanderjwestley@gmail.com?subject=AJ Westley Contact Form" className="navbar__links">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+        </nav>
     )
 }
