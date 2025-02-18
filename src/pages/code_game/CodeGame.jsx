e9b25c1a095c90509b44f4b3893ecabb2e66b837
Alexander Jonathan Westley <24814245@sun.ac.za>
2025-02-18 22:24:34 +0200

    Added code functionality

--- src/pages/code_game/CodeGame.jsx
+++ src/pages/code_game/CodeGame.jsx
@@ -2,6 +2,10 @@ import { NavBar } from '../../components/Nav';
 import { Footer } from '../../components/Footer';
 import '../../styles/code_game.css'
 
+var alphabet = "abcdefghijklmnopqrstuvwxyz0123456789".split('');
+var shuffled = [...alphabet];
+var code = {}
+
 export function GamePage() {
     return (
         <>
@@ -29,4 +33,40 @@ function Game() {
             </div>
         </div>
     )
+}
+
+function generate_code(input_text) {
+    shuffle(shuffled);
+    let input_array = input_text.toLowerCase().split('');
+    for (let i = 0; i < input_array.length; i++) {
+        let found = false;
+        for (let j = 0; j < alphabet.length; j++) {
+            if (input_array[i] === alphabet[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            return `Sorry, ${input_array[i]} is an invalid character!\nPlease use only the digits 0-9 or letters a-z.`
+    }
+
+    alphabet.forEach(function (k, i) {
+        code[k] = shuffled[i];
+    })
+}
+
+function shuffle(array) {
+    let currentIndex = array.length;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
 }
