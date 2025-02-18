d4efbaae4b937d96dab9f481be90960f154c42b8
Alexander Jonathan Westley <24814245@sun.ac.za>
2025-02-18 21:42:42 +0200

    Added textarea

--- src/pages/code_game/CodeGame.jsx
+++ src/pages/code_game/CodeGame.jsx
@@ -16,7 +16,10 @@ function Game() {
     return (
         <div className="code_game">
             <div className="code_game__container">
-                
+                <h1>Code Cracker!</h1>
+                <textarea className="code_input__textarea" rows="4" cols="50">
+                    
+                </textarea>
             </div>
         </div>
     )
