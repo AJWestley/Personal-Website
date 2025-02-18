0957e3f0a73da85eaee0aad6a6876a9b6e0ae21a
Alexander Jonathan Westley <24814245@sun.ac.za>
2025-02-18 22:01:33 +0200

    Added output fields

--- src/pages/code_game/CodeGame.jsx
+++ src/pages/code_game/CodeGame.jsx
@@ -16,9 +16,15 @@ function Game() {
     return (
         <div className="code_game">
             <div className="code_game__container">
-                <h1 className='code_game__header'>Code Cracker!</h1>
+                <h1>Code Cracker!</h1>
+                <h2>Enter secret message:</h2>
                 <textarea className="code_input__textarea" rows="4" cols="50">
-                    
+                </textarea>
+                <h2 id='disappearing'>The secret code:</h2>
+                <textarea id='disappearing' className="secret_code__textarea" rows="4" cols="50">
+                </textarea>
+                <h2 id='disappearing'>The encoded message:</h2>
+                <textarea id='disappearing' className="encoded_message__textarea" rows="4" cols="50">
                 </textarea>
             </div>
         </div>
